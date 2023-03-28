7b2cb273ebdfbd4818182ae7b641792c404fefa7
ksh25 <ksh251024@naver.com>
2023-03-28 19:54:03 +0900

    버튼 네개 작동함

--- app/src/main/java/kr/hs/emirim/sohee/code_layout/SecondActivity.kt
+++ app/src/main/java/kr/hs/emirim/sohee/code_layout/SecondActivity.kt
@@ -1,11 +1,39 @@
 package kr.hs.emirim.sohee.code_layout
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var btn1 : Button
+    lateinit var btn2 : Button
+    lateinit var btn3 : Button
+    lateinit var btn4 : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        btn1=findViewById(R.id.btn1)
+        btn2=findViewById(R.id.btn2)
+        btn3=findViewById(R.id.btn3)
+        btn4=findViewById(R.id.btn4)
+
+        btn1.setOnClickListener{
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"))
+            startActivity(intent)
+        }
+        btn2.setOnClickListener{
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse("tel:/010-3333-7777"))
+            startActivity(intent)
+        }
+        btn3.setOnClickListener{
+            val intent=Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"))
+            startActivity(intent)
+        }
+        btn4.setOnClickListener{
+        finish()
+        }
     }
 }
